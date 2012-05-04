6602ee1d706078a9560d1d9225b19ac0a9e7f006
Vijay Chavan <vijaychavan@SIYAULHAQUs-Mac-mini.local>
2012-05-04 10:42:53 +0530

    Initial Commit

---
+++ MultipleCarousel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultipleCarousel
+//
+//  Created by Vijay Chavan on 04/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MultipleCarousel/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  MultipleCarousel
+//
+//  Created by Vijay Chavan on 04/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
